3a84eed3d0b86bc88030ec984f6fdac3434582d1
HassanBahati <mukisabahati@gmail.com>
2022-10-24 09:19:09 +0300

    intial commit

---
+++ OOP-Lab-Exercise-Inheritance/Lawyer.java
@@ -0,0 +1,16 @@
+
+// A class to represent lawyers.
+
+public class Lawyer extends Employee {
+    public int getVacationDays() {
+        return super.getVacationDays() + 5;           // 3 weeks vacation
+    }
+
+    public String getVacationForm() {
+        return "pink";
+    }
+    
+    public void sue() {
+        System.out.println("I'll see you in court!");
+    }
+}
---
+++ OOP-Lab-Exercise-Inheritance/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+		Truck mycar = new Truck();
+        System.out.println(mycar);mycar.m1();
+        mycar.m2();
+	}
+}
